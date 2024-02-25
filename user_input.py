1bb7e1a5af4ee2263bcfba3e90b4b55151a79d82
Fredy2024 <fredobonyo5@gmail.com>
2024-02-26 00:24:54 +0300

    week one python assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print= ("""hello {JJ}"! You are {26} years old and your location is {Nairobi}""") 
