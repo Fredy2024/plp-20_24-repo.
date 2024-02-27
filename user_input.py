58f9e76cdb6ee05d9eafb736fa6b26ab28472458
Fredy2024 <159558972+Fredy2024@users.noreply.github.com>
2024-02-27 05:01:56 +0000

    Add files via upload

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name = input("what is your name: ?")
 age = input("what is your age: ?")
 location = input("what is  your location: ?")
 
-print("your name i "+ name + "your age is "+ age + "and your location is "+ location") 
+print("your name is "+ name + "your age is "+ age + "and your location is "+ location") 
